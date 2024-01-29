dcb7b1c2e816c0ad84e839ffd91c2e449495b89d
anti-pc <buketculfaoglu@gmail.com>
2024-01-30 01:23:18 +0300

    added second receiver and time delays

--- RabbitReceiver2/Program.cs
+++ RabbitReceiver2/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 ConnectionFactory factory = new();
 factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
-factory.ClientProvidedName = "Rabbit Receiver1 App";
+factory.ClientProvidedName = "Rabbit Receiver2 App";
 
 IConnection con = factory.CreateConnection();
 
@@ -24,7 +24,7 @@ channel.BasicQos(0, 1, false);
 var consumer = new EventingBasicConsumer(channel);
 consumer.Received += (sender, args) =>
 {
-    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
     var body = args.Body.ToArray();
 
     string message = Encoding.UTF8.GetString(body);
